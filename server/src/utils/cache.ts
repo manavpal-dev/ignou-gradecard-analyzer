6469d0c15fa703a80112f1c8b78c1d8d34b9d4a3
Manav Pal <manav96544@gmail.com>
2026-03-28 16:25:06 +0530

    reduced cache-duration timing to 1 hour

--- server/src/utils/cache.ts
+++ server/src/utils/cache.ts
@@ -4,7 +4,7 @@ type CacheData = {
 };
 
 const cache = new Map<string, CacheData>();
-const CACHE_DURATION = 24 * 60 * 60 * 1000; // 24 hours
+const CACHE_DURATION = 1 * 60 * 60 * 1000; // 1 hours
 
 export const getCache = (key: string) => {
   const data = cache.get(key);
